d30e1447489c215fe708caaf0bd6f4f9a0b408eb
AlinaFedorchuk <a.lina.3092@gmail.com>
2015-12-17 01:54:33 +0200

    app file

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Alina on 14.12.2015.
+ */
+var express = require('express');
+var request = require('request');
+var cheerio = require("cheerio");
+var app = express();
+
+app.get('/', function (req, res) {
+	request('http://www.gsmarena.com/lenovo_vibe_x3_c78-7756.php ', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	
+		var $ = cheerio.load(body);
+		var description = $("div .main-review").html();
+		
+		/*function Translate(){
+			var text = $("div .main-review").text();
+			var pattern = /network/ig;
+			var NewText = text.replace (pattern, function replacer(match){
+				return match.toUpperCase();
+			});
+			return NewText;
+		}*/
+		
+			res.send(description); 
+			 
+		
+	  }
+	  else {
+		res.send("We’ve encountered an error: " + error);
+	}
+
+}) 
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://127.0.0.1', host, port);
+
+});
